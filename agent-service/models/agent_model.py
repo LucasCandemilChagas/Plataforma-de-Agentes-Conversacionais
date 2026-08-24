64543dfdbe5fad0d58165885893391dda0ad3b8c
Lucas Chagas <82179740+LucasCandemilChagas@users.noreply.github.com>
2026-08-24 15:52:29 -0300

    fix: table name agent

--- agent-service/models/agent_model.py
+++ agent-service/models/agent_model.py
@@ -5,4 +5,6 @@ class AgentResponseBase(SQLModel):
     perguntas : str = Field(max_length=256)
 
 class AgentModel(AgentResponseBase, table=True):
+    __tablename__ = 'agentresponses'
+    
     id : Optional[int] = Field(default=None, sa_column=Column(Integer, Identity(start=1, cycle=False), primary_key=True))
