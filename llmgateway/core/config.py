d25092929f78efffb66c1dfd0291bed0a838a88e
Lucas Chagas <82179740+LucasCandemilChagas@users.noreply.github.com>
2026-09-15 17:56:28 -0300

    feat: New Default Model

--- llmgateway/core/config.py
+++ llmgateway/core/config.py
@@ -4,6 +4,7 @@ class Settings(BaseSettings):
 
     API_V1_STR : str= '/api/v1'
     OLLAMA_BASE_URL: str = "http://localhost:11434"
+    DEFAULT_MODEL: str = "ollama/phi3"
     
     class Config:
         env_file = ".env"
